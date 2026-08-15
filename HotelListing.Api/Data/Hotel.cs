8247b08134c0dc8b33e9933009a7c899886af7b2
Sahil Solkar <sahilsolkar@benchmarkit.solutions>
2026-08-15 15:32:49 +0530

    Add EF Core with Country & Hotel entities, init migration
    Introduced Entity Framework Core to the project. Defined `Country` and `Hotel` entities with a one-to-many relationship. Added `HotelListingDBContext` for entity management. Updated project references for EF Core, SQL Server, and migrations. Created initial migration for database schema. Added connection string to `appsettings.json` and registered the DB context in the DI container.

--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -1,11 +1,14 @@
-﻿namespace HotelListing.Api.Data
-{
-    public class Hotel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+﻿namespace HotelListing.Api.Data;
 
-        public double Rating { get; set; }
-    }
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+
+    public double Rating { get; set; }
+
+    public int CountryId { get; set; }
+
+    public Country? Country { get; set; }
 }
---
+++ HotelListing.Api/Data/HotelListingDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Api.Data;
+
+public class HotelListingDBContext:DbContext
+{
+    public HotelListingDBContext(DbContextOptions options) : base(options)
+    { 
+    }
+
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+
+}
